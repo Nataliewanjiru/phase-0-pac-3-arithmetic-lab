f13b826ebd37c5bb46d27d4912bfea96258bf739
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-07 23:46:05 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b){
+  return(a+b);
+}
+
+let subtract = (a,b) =>{
+   return(a-b);
+}
+
+let multiply = (a,b) =>{
+  return(a*b);
+}
+
+let divide = (a,b) =>{
+return(a/b);
+}
+
+let increment = n =>{
+    return ++n ;
+}
+
+let decrement = n =>{
+    return --n ;
+}
+
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
